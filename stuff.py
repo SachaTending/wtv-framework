c5e633fe733db11ac21fb243cee28d143166e593
SachaTending <sasasabae679@gmail.com>
2023-05-20 00:13:23 +0000

    star.html: create service unavaible page stuff.py: add wtv-star service

--- stuff.py
+++ stuff.py
@@ -1,11 +1,13 @@
 from wtvframework import *
 from glob import glob
+import config
 
 imgs = Service("wtv-images")
 content_svc = Service("wtv-content")
 wtv_center_svc = Service("wtv-center")
 favorite_svc = Service("wtv-favorite")
-svcs = [imgs, content_svc, wtv_center_svc, favorite_svc]
+star_svc = Service("wtv-star")
+svcs = [imgs, content_svc, wtv_center_svc, favorite_svc, star_svc]
 
 @imgs.addhandl("splash")
 def splash_img(data):
@@ -15,6 +17,11 @@ def splash_img(data):
 @wtv_center_svc.addhandl("money")
 def money_handl(data):
     return Responce(200, data=open("pages/wtv-center/money.html").read())
+
+# wtv-star
+@star_svc.addhandl("star")
+def star_handl(data):
+    return Responce(200, data=open("pages/wtv-star/star.html").read().replace("NETWOK_NAME_PLACEHOLDER", config.name))
 
 # Add ROMCache
 for i in glob("d/content/ROMCache/*"): content_svc.addfile(i.removeprefix("d/content/"), SendFile(i))
